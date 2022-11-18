1577ee25c99877d7bbd15eba461fdd6883437fc7
Kristina Lazarevic <96001243+kiks717@users.noreply.github.com>
2022-11-18 20:36:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,30 +23,18 @@ function App() {
   //za modal 
 
   const onAdd = (product) => {
-      /*ova funkcija prihvata elemnt koji treba biti dodan i pomocu 
-  exist cemo provjeriti da li se u cartItems nalazi product sa
-  jednakim id-em kao id u array of products koji treba biti dodan
-  */
+
     const exist = cartItems.find((x) => x.id === product.id);
     if (exist) {
       setCartItems(
-        /*updejtujem, radim to u setCartItems,
-        ako nadjem product trebam uvecati njegovu vrijednost
-        mapiram,dobijam novi niz, dobijajuci novi niz, treba da
-        ocuvam stari content gdje cu na postojecu vrijednost da 
-        dodam novu, a ako je neki drugi poizvod ne treba mijenjati  */
         cartItems.map((x) =>
           x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
         )
       );
     } else {
-      /*ako poizvod ne postoji u korpi, koristim arrayConcat
-      merge arrays(based on previous state array)
-      spajanje nizova i quantity of product at beggining is 1 */
-      setCartItems([...cartItems, { ...product, qty: 1 }]);
-      //pass function here and on AddToCart buttons
 
-      //onAdd prosledjujem kroz 3 komponente
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+
     }
   };
 
@@ -64,23 +52,13 @@ function App() {
       );
     }
   };
-/*na najvisem nivou apliakcije ide router 
-routes(switch)  odlucuje gdje u router sistemu zelimo da imamo rute
-ne mozemo koristii router ako je izvan router componente
-*/
+
    return (
     <BrowserRouter>
     <GlobalStyle/>
     <Hero/> 
     <UseModal setIsOpen={setIsOpen} id="cart"/>
-{/* Modal is children of some other components
-    and we forced child component to render somewhere outside
-    of parent component (div id="portal")
 
-    all events of this modal are going to 
-    delegate up to  parents of this modal
-    {when we click on button it's going to }
- */}
       <Modal
       id="cart"
       open={isOpen} 
